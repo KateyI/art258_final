d15076abfe303d4a8f4c007d47df595e61c755b5
KateyI <kinouye4@hawaii.edu>
2016-12-14 23:47:46 -1000

    added third page and one libaray

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // put all your JS in here
+
+(function(){
+  var grid;
+  function init() {
+    grid = new Minigrid({
+      container: '.cards',
+      item: '.card',
+      gutter: 12
+    });
+    grid.mount();
+  }
+
+  // mount
+  function update() {
+    grid.mount();
+  }
+
+  document.addEventListener('DOMContentLoaded', init);
+  window.addEventListener('resize', update);
+})();
